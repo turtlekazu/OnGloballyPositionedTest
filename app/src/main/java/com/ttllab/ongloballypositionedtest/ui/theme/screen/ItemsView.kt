879fe9b5005b9fc43f2d317d2a1136f437cc797b
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2024-11-09 14:17:03 +0900

    add zooming check

--- app/src/main/java/com/ttllab/ongloballypositionedtest/ui/theme/screen/ItemsView.kt
+++ app/src/main/java/com/ttllab/ongloballypositionedtest/ui/theme/screen/ItemsView.kt
@@ -1,27 +1,39 @@
 package com.ttllab.ongloballypositionedtest.ui.theme.screen
 
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 
 @Composable
 fun ItemsView(
     pageIndex: Int,
     modifier: Modifier = Modifier,
 ) {
+    var scale by remember { mutableFloatStateOf(1f) }
+
     Box(
         modifier = modifier
             .fillMaxSize()
-//            .graphicsLayer { // Uncomment this for scaling check
-//                scaleX = 2f
-//                scaleY = 2f
+//            .pointerInput(Unit) { // Uncomment to check zooming
+//                detectTransformGestures { _, _, zoom, _ ->
+//                    scale *= zoom
+//                    scale = scale.coerceAtLeast(0.7f)
+//                }
 //            }
-//            .scale(2f) // Uncomment this for scaling check
+//            .graphicsLayer {
+//                scaleX = scale
+//                scaleY = scale
+//            }
     ) {
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
