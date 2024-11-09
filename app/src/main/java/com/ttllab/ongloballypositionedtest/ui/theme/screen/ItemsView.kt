38958ca8a0d07188e43fa1e69290f7cab3cc6304
Kazuhiro ISHIBASHI <turtlekazu@icloud.com>
2024-11-09 15:13:24 +0900

    add zooming and panning feature

--- app/src/main/java/com/ttllab/ongloballypositionedtest/ui/theme/screen/ItemsView.kt
+++ app/src/main/java/com/ttllab/ongloballypositionedtest/ui/theme/screen/ItemsView.kt
@@ -8,11 +8,16 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ItemsView(
@@ -20,19 +25,31 @@ fun ItemsView(
     modifier: Modifier = Modifier,
 ) {
     var scale by remember { mutableFloatStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val configuration = LocalConfiguration.current
+    val density = LocalDensity.current
+    val screenWidth = with(density) { configuration.screenWidthDp.dp.toPx() }
+    val screenHeight = with(density) { configuration.screenHeightDp.dp.toPx() }
 
     Box(
         modifier = modifier
             .fillMaxSize()
-//            .pointerInput(Unit) { // Uncomment to check zooming
-//                detectTransformGestures { _, _, zoom, _ ->
-//                    scale *= zoom
-//                    scale = scale.coerceAtLeast(0.7f)
+//            .pointerInput(pageIndex) { // Uncomment to check zooming
+//                detectTransformGestures { _, pan, zoom, _ ->
+//                    scale = (scale * zoom).coerceAtLeast(1f)
+//                    val boundsX = (screenWidth * scale - screenWidth) / 2
+//                    val boundsY = (screenHeight * scale - screenHeight) / 2
+//                    offset = Offset(
+//                        x = (offset.x + pan.x).coerceIn(-boundsX, boundsX),
+//                        y = (offset.y + pan.y).coerceIn(-boundsY, boundsY),
+//                    )
 //                }
 //            }
 //            .graphicsLayer {
 //                scaleX = scale
 //                scaleY = scale
+//                translationX = offset.x
+//                translationY = offset.y
 //            }
     ) {
         LazyVerticalGrid(
